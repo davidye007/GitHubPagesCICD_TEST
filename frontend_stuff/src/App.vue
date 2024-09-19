419ff835d27d759230a8c4dd899b7ab6349dd63e
davidye007 <davidyeap@outlook.com>
2024-09-19 16:49:09 -0700

    test

--- frontend_stuff/src/App.vue
+++ frontend_stuff/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Rocky Mountain Rules!!!"/>
+    <HelloWorld msg="Trials and Tribulations"/>
   </div>
 </template>
 
