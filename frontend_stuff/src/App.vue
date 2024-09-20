9fc58f8af37e9c17cccc362ad1e936db3a551e70
davidye007 <davidyeap@outlook.com>
2024-09-19 17:02:42 -0700

    test again

--- frontend_stuff/src/App.vue
+++ frontend_stuff/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Trials and Tribulations"/>
+    <HelloWorld msg="Trials and Tribulations!"/>
   </div>
 </template>
 
